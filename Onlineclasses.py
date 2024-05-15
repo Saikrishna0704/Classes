30afd6f9f18681ce91c279f008ee3b92114e89e8
Saikrishna0704 <83568778+Saikrishna0704@users.noreply.github.com>
2024-05-15 01:30:35 -0400

    Update and rename Onlineclasses to Onlineclasses.py

--- Onlineclasses.py
+++ Onlineclasses.py
@@ -1,3 +1,5 @@
+import streamlit as st
+import time
 def main():
     st.sidebar.image("https://play-lh.googleusercontent.com/r1CHZYUHjyTu2iwFhsYb1q5DLD2zmA1nQJssbSNKYAtvJOlEG_-kO4VTjyraLIIs8g", use_column_width=True)
     st.sidebar.write("Online Classes")
@@ -11,7 +13,6 @@ def main():
             st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4F9lch1Lq12oVIdwdV6gDZBlph-maWw6kFK7mZvffI06GBuMVxQE9faROTw&s")
             st.markdown("<h1 style='text-align: center; color: blue;'>Large Text</h1>", unsafe_allow_html=True)
             st.write("Eddie Kareakka C batch bugga")
-
 if __name__ == "__main__":
     main()
 
